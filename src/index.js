56f7b57c11976a227f4121d31b43f09630b71ac7
xmartinezpujol <xavier.martinez@rakuten.com>
2019-08-28 00:16:29 +0200

    Update: Exported components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Button from './Button';
+import Card from './Card';
 import Carousel from './Carousel';
+import CarouselHero from './CarouselHero';
+import Icon from './Icon';
+import Input from './Input';
+import Loader from './Loader';
+import Modal from './Modal';
+import Text from './Text';
+import View from './View';
 
-export {
-  Button,
-  Carousel,
-};
+export { Button, Card, Carousel, CarouselHero, Icon, Input, Loader, Modal, Text, View };
