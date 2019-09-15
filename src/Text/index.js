d722d12e0bff8ed35fb2f5a1f90d7952b62d20bd
xmartinezpujol <xavier.martinez@rakuten.com>
2019-09-15 18:42:54 +0200

    Update - Text components can now be styled with styled-component className

--- src/Text/index.js
+++ src/Text/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Text = (props) => {
+const Text = props => {
   const styles = {
     h1: {
       fontSize: 20,
@@ -9,28 +9,28 @@ const Text = (props) => {
       fontWeight: 900,
       '& strong': { color: props.accentColor },
       '@media(min-width: 768px)': {
-        fontSize: 24,
+        fontSize: 24
       },
       '@media(min-width: 992px)': {
-        fontSize: 28,
+        fontSize: 28
       },
       '@media(min-width: 1200px)': {
-        fontSize: 36,
-      },
+        fontSize: 36
+      }
     },
     'h1.w': {
       fontSize: 20,
       color: '#FFFFFF',
       fontWeight: 900,
       '@media(min-width: 768px)': {
-        fontSize: 24,
+        fontSize: 24
       },
       '@media(min-width: 992px)': {
-        fontSize: 28,
+        fontSize: 28
       },
       '@media(min-width: 1200px)': {
-        fontSize: 36,
-      },
+        fontSize: 36
+      }
     },
     h2: {
       fontSize: 16,
@@ -38,56 +38,56 @@ const Text = (props) => {
       fontWeight: 900,
       '& strong': { color: props.accentColor },
       '@media(min-width: 768px)': {
-        fontSize: 18,
+        fontSize: 18
       },
       '@media(min-width: 992px)': {
-        fontSize: 22,
+        fontSize: 22
       },
       '@media(min-width: 1200px)': {
-        fontSize: 25,
-      },
+        fontSize: 25
+      }
     },
     'h2.1': {
       fontSize: 16,
       color: '#36424A',
       fontWeight: 700,
       '@media(min-width: 768px)': {
-        fontSize: 18,
+        fontSize: 18
       },
       '@media(min-width: 992px)': {
-        fontSize: 22,
+        fontSize: 22
       },
       '@media(min-width: 1200px)': {
-        fontSize: 25,
-      },
+        fontSize: 25
+      }
     },
     'h2.2': {
       fontSize: 16,
       color: '#6E7A83',
       fontWeight: 700,
       '@media(min-width: 768px)': {
-        fontSize: 18,
+        fontSize: 18
       },
       '@media(min-width: 992px)': {
-        fontSize: 22,
+        fontSize: 22
       },
       '@media(min-width: 1200px)': {
-        fontSize: 25,
-      },
+        fontSize: 25
+      }
     },
     'h2.w': {
       fontSize: 16,
       color: '#FFFFFF',
       fontWeight: 900,
       '@media(min-width: 768px)': {
-        fontSize: 18,
+        fontSize: 18
       },
       '@media(min-width: 992px)': {
-        fontSize: 22,
+        fontSize: 22
       },
       '@media(min-width: 1200px)': {
-        fontSize: 25,
-      },
+        fontSize: 25
+      }
     },
     h3: {
       fontSize: 14,
@@ -95,42 +95,42 @@ const Text = (props) => {
       fontWeight: 900,
       '& strong': { color: props.accentColor },
       '@media(min-width: 768px)': {
-        fontSize: 14,
+        fontSize: 14
       },
       '@media(min-width: 992px)': {
-        fontSize: 16,
+        fontSize: 16
       },
       '@media(min-width: 1200px)': {
-        fontSize: 18,
-      },
+        fontSize: 18
+      }
     },
     'h3.1': {
       fontSize: 14,
       color: '#6E7A83',
       fontWeight: 700,
       '@media(min-width: 768px)': {
-        fontSize: 14,
+        fontSize: 14
       },
       '@media(min-width: 992px)': {
-        fontSize: 16,
+        fontSize: 16
       },
       '@media(min-width: 1200px)': {
-        fontSize: 18,
-      },
+        fontSize: 18
+      }
     },
     'h3.w': {
       fontSize: 14,
       color: '#FFFFFF',
       fontWeight: 700,
       '@media(min-width: 768px)': {
-        fontSize: 14,
+        fontSize: 14
       },
       '@media(min-width: 992px)': {
-        fontSize: 16,
+        fontSize: 16
       },
       '@media(min-width: 1200px)': {
-        fontSize: 18,
-      },
+        fontSize: 18
+      }
     },
     h4: {
       fontSize: 10,
@@ -138,28 +138,28 @@ const Text = (props) => {
       fontWeight: 400,
       '& strong': { color: props.accentColor },
       '@media(min-width: 768px)': {
-        fontSize: 12,
+        fontSize: 12
       },
       '@media(min-width: 992px)': {
-        fontSize: 14,
+        fontSize: 14
       },
       '@media(min-width: 1200px)': {
-        fontSize: 16,
-      },
+        fontSize: 16
+      }
     },
     'h4.w': {
       fontSize: 10,
       color: '#FFFFFF',
       fontWeight: 400,
       '@media(min-width: 768px)': {
-        fontSize: 12,
+        fontSize: 12
       },
       '@media(min-width: 992px)': {
-        fontSize: 14,
+        fontSize: 14
       },
       '@media(min-width: 1200px)': {
-        fontSize: 16,
-      },
+        fontSize: 16
+      }
     },
     h5: {
       fontSize: 11,
@@ -167,85 +167,78 @@ const Text = (props) => {
       textTransform: 'uppercase',
       '& strong': { color: props.accentColor },
       '@media(min-width: 768px)': {
-        fontSize: 16,
+        fontSize: 16
       },
-      '@media(min-width: 992px)': {
-
-      },
-      '@media(min-width: 1200px)': {
-
-      },
+      '@media(min-width: 992px)': {},
+      '@media(min-width: 1200px)': {}
     },
     'h5.w': {
       fontSize: 16,
       fontWeight: 900,
       textTransform: 'uppercase',
-      color: '#FFFFFF',
+      color: '#FFFFFF'
     },
     p1: {
       fontSize: 16,
       color: '#36424A',
       fontWeight: 300,
       whiteSpace: 'pre-line',
-      '& strong': { color: props.accentColor },
+      '& strong': { color: props.accentColor }
     },
     'p1.w': {
       fontSize: 16,
       color: '#FFFFFF',
       fontWeight: 300,
-      whiteSpace: 'pre-line',
+      whiteSpace: 'pre-line'
     },
     p2: {
       fontSize: 13,
       color: '#B6BABD',
-      fontWeight: 400,
+      fontWeight: 400
     },
     p3: {
       fontSize: 12,
       color: '#B6BABD',
       letterSpacing: '1px',
       textTransform: 'uppercase',
-      fontWeight: 400,
+      fontWeight: 400
     },
     p4: {
       fontSize: 12,
       color: '#B6BABD',
-      fontWeight: 400,
+      fontWeight: 400
     },
     p5: {
       fontSize: 12,
       color: '#6E7A83',
-      fontWeight: 400,
+      fontWeight: 400
     },
     span: {
       fontSize: 16,
       color: '#36424A',
       fontWeight: 300,
       whiteSpace: 'pre-line',
-      margin: '5px 0',
-    },
+      margin: '5px 0'
+    }
   };
   let propOrDefault = '';
 
   if (props.type.substring(0, 1) === 'h') {
-    propOrDefault = typeof (styles[props.type]) !== 'undefined'
-      ? props.type.substring(0, 2)
-      : styles.p1;
+    propOrDefault =
+      typeof styles[props.type] !== 'undefined' ? props.type.substring(0, 2) : styles.p1;
   }
   if (props.type.substring(0, 1) === 'p') {
-    propOrDefault = typeof styles[props.type] !== 'undefined'
-      ? props.type.substring(0, 1)
-      : styles.p1;
+    propOrDefault =
+      typeof styles[props.type] !== 'undefined' ? props.type.substring(0, 1) : styles.p1;
   }
   if (props.type.substring(0, 1) === 's') {
-    propOrDefault = typeof styles[props.type] !== 'undefined'
-      ? props.type.substring(0, 4)
-      : styles.p1;
+    propOrDefault =
+      typeof styles[props.type] !== 'undefined' ? props.type.substring(0, 4) : styles.p1;
   }
 
   const TextGenerator = styled(propOrDefault)(styles[props.type]);
   return (
-    <TextGenerator style={props.style}>
+    <TextGenerator style={props.style} className={props.className}>
       {props.children}
     </TextGenerator>
   );
--- src/stories/Text.js
+++ src/stories/Text.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { storiesOf } from '@storybook/react';
 
 import Text from '../Text';
 import View from '../View';
 
-storiesOf('Texts', module)
-  .add('local', () => (
-    <View padding={50} type="rice" direction="column">
-      <Text type="h1">H1 - Im a title, trying to be descriptive!</Text>
-      <Text type="h2">H2 - Im a title, trying to be descriptive!</Text>
-      <Text type="h3">H3 - Im a title, trying to be descriptive!</Text>
-      <Text type="h4">H4 - Im a title, trying to be descriptive!</Text>
-      <Text type="h5">H4 - Im a title, trying to be descriptive!</Text>
-      <Text type="p1">P1 - Im a title, trying to be descriptive!</Text>
-      <Text type="p2">P2 - Im a title, trying to be descriptive!</Text>
-      <Text type="p3">P3 - Im a title, trying to be descriptive!</Text>
-      <Text type="p4">P4 - Im a title, trying to be descriptive!</Text>
-      <Text type="p5">P5 - Im a title, trying to be descriptive!</Text>
-      <Text type="span">span - Im a title, trying to be descriptive!</Text>
-    </View>
-  ));
+const StyledText = styled(Text)`
+  color: #00e676;
+`;
+
+storiesOf('Texts', module).add('local', () => (
+  <View padding={50} type="rice" direction="column">
+    <Text type="h1">H1 - Im a title, trying to be descriptive!</Text>
+    <Text type="h2">H2 - Im a title, trying to be descriptive!</Text>
+    <Text type="h3">H3 - Im a title, trying to be descriptive!</Text>
+    <Text type="h4">H4 - Im a title, trying to be descriptive!</Text>
+    <Text type="h5">H4 - Im a title, trying to be descriptive!</Text>
+    <Text type="p1">P1 - Im a title, trying to be descriptive!</Text>
+    <Text type="p2">P2 - Im a title, trying to be descriptive!</Text>
+    <Text type="p3">P3 - Im a title, trying to be descriptive!</Text>
+    <Text type="p4">P4 - Im a title, trying to be descriptive!</Text>
+    <Text type="p5">P5 - Im a title, trying to be descriptive!</Text>
+    <Text type="span">span - Im a title, trying to be descriptive!</Text>
+    <StyledText type="span">Styled - Im a styled-component Text!</StyledText>
+  </View>
+));
